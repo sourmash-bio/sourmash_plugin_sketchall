fb3c58416827ac62c2d30276f47a808dd8d28d74
C. Titus Brown <titus@idyll.org>
2023-01-22 07:11:04 -0800

    working

--- src/sketchall.snakefile
+++ src/sketchall.snakefile
@@ -1,17 +1,30 @@
-print('XXX', config['dir'])
+import os
+from sourmash.logging import debug_literal, set_quiet
 
-genomes, = glob_wildcards(f"{config['dir']}/{{g}}")
+print(config['debug'])
+debug = bool(int(config['debug']))
+set_quiet(False, debug)
 
-# remove .sig files
-genomes = [ g for g in genomes if not g.endswith('.sig') ]
+DIRNAME = config['location']
+debug_literal(f"sketchall: running on '{DIRNAME}'")
+
+# find all files that don't end in .sig.gz or .sig, run on 'em
+FILES = []
+for root, dirs, files in os.walk(DIRNAME, topdown=False):
+    for name in files:
+        if not name.endswith('.sig') and not name.endswith('.sig.gz'):
+            filename = os.path.join(root, name)
+            FILES.append(filename + '.sig.gz')
+
+debug_literal(f"found {len(FILES)} files.")
 
 rule all:
     input:
-        expand(f"{config['dir']}/{{g}}.sig", g=genomes)
+        FILES
 
 rule sketch_wc:
     input: "{genome}"
-    output: "{genome}.sig"
+    output: "{genome}.sig.gz"
     threads: 1
     shell: """
        sourmash sketch dna {input} -o {output}
--- src/sourmash_plugin_sketchall.py
+++ src/sourmash_plugin_sketchall.py
@@ -1,13 +1,17 @@
 #! /usr/bin/env python
 """
 @@
+
+IDEAS:
+- support taking in a CSV or file list instead?
+- support ignoring or limiting to a particular extension
 """
 import os
 import subprocess
 import sys
-import argparse
 import sourmash
-from sourmash import sourmash_args, plugins
+from sourmash import plugins
+from sourmash.logging import debug_literal
 
 
 class Command_SketchAll(plugins.CommandLinePlugin):
@@ -16,14 +20,22 @@ class Command_SketchAll(plugins.CommandLinePlugin):
 
     def __init__(self, p):
         super().__init__(p)
-        p.add_argument('directory')
-        p.add_argument('-j', '-c', '--cores', type=int, default=4)
+        p.add_argument('location')
+        p.add_argument('-j', '-c', '--cores', type=int, default=4,
+                       help="number of processes/cores to use")
+        p.add_argument('-n', '--dry-run', '--dryrun', action='store_true',
+                       help="only print out what would be run, do not execute")
 
     def main(self, args):
         super().main(args)
-        directory = args.directory
+        debug_literal(f"sketchall main: location is {args.location}")
+        print(sys.executable)
 
-        run_snakemake(config_params=[f"dir={args.directory}",])
+        debug = 1 if args.debug else 0
+
+        run_snakemake(config_params=[f"location={args.location}",
+                                     f"debug={debug}"],
+                      cores=args.cores, dry_run=args.dry_run)
 
 
 def get_snakefile_path(name):
@@ -33,31 +45,31 @@ def get_snakefile_path(name):
 
 
 def run_snakemake(*,
-    snakefile_name="sketchall.snakefile",
-    config_params=[],
-    extra_args=[],
-    subprocess_args=None
+                  snakefile_name="sketchall.snakefile",
+                  config_params=[],
+                  extra_args=[],
+                  subprocess_args=None,
+                  cores=1,
+                  dry_run=False,
 ):
     # find the Snakefile relative to package path
     snakefile = get_snakefile_path(snakefile_name)
 
     # basic command
-    cmd = ["snakemake", "-s", snakefile]
+    cmd = [sys.executable, "-m", "snakemake", "-s", snakefile]
 
-    # snakemake sometimes seems to want a default -j; set it to 1 for now.
-    # can overridden later on command line. #@CTB
-    cmd += ["-j", "1", '-k']
+    cmd += ["-j", str(cores), '-k']
 
     # add rest of snakemake arguments
     cmd += list(extra_args)
 
-    # add config params, and --outdir
+    # add config params
     config_params = list(config_params)
 
     if config_params:
         cmd += ["--config", *config_params]
 
-    print("final command:", cmd)
+    debug_literal(f"sketchall: snakemake command is {repr(cmd)}")
 
     # runme
     if subprocess_args is None:
